1e25dc9fbc8885b7b1aebf5bd75595e611abbdd8
Devi Krishna Prasad <s.c.krishna.prasad@accenture.com>
2025-03-13 23:47:13 +0530

    minor changes

--- arrays/two_sum.py
+++ arrays/two_sum.py
@@ -1,4 +1,5 @@
 #https://leetcode.com/problems/two-sum/
+#return indices of two two number which sum up to result
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
